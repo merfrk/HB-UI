cbb218bf740b394c434b5733d3d88a8a20e9d519
Ömer Faruk ARTAN <o.farukartan@hotmail.com>
2022-08-25 11:34:34 +0300

    create middle section

--- HB UI/LogoViewController.swift
+++ HB UI/LogoViewController.swift
@@ -2,7 +2,7 @@
 //  LogoViewController.swift
 //  HB UI
 //
-//  Created by Omer on 21.08.2022.
+//  Created by Omer on 22.08.2022.
 //
 
 import UIKit
@@ -12,38 +12,12 @@ class LogoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
-//        let image1 = UIImage(named: "hblogo")
-//        let image2 = resizeImage(image: image1!, targetSize: CGSize(width: 40.0, height: 40.0))
+        var customTabBarItem:UITabBarItem = UITabBarItem(title: nil, image: UIImage(named: "hblogo")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal), selectedImage: UIImage(named: "hblogo"))
+        self.tabBarController?.tabBar.items![4] = customTabBarItem
         
         
     }
     
 
-    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage? {
-        let size = image.size
-        
-        let widthRatio  = targetSize.width  / size.width
-        let heightRatio = targetSize.height / size.height
-        
-        // Figure out what our orientation is, and use that to form the rectangle
-        var newSize: CGSize
-        if(widthRatio > heightRatio) {
-            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
-        } else {
-            newSize = CGSize(width: size.width * widthRatio, height: size.height * widthRatio)
-        }
-        
-        // This is the rect that we've calculated out and this is what is actually used below
-        let rect = CGRect(origin: .zero, size: newSize)
-        
-        // Actually do the resizing to the rect using the ImageContext stuff
-        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
-        image.draw(in: rect)
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return newImage
-    }
-
+    
 }
--- HB UI/ViewController.swift
+++ HB UI/ViewController.swift
@@ -10,12 +10,45 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var searchView: UIView!
+    @IBOutlet var konumView: UIView!
+    @IBOutlet var hepsiPayView: UIView!
+    
+    @IBOutlet var premiumImageView: UIImageView!
+    @IBOutlet var okulImageView: UIImageView!
+    @IBOutlet var modeImageView: UIImageView!
+    @IBOutlet var tekImageView: UIImageView!
+    @IBOutlet var anneImageView: UIImageView!
+    
+    @IBOutlet var indirimView: UIView!
+    @IBOutlet var fırsatView: UIView!
+    @IBOutlet var reklamImageView: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         searchView.layer.borderWidth = 0.5
         searchView.layer.borderColor = UIColor.lightGray.cgColor
         searchView.layer.cornerRadius = 8
+        
+        konumView.layer.cornerRadius = 8
+        hepsiPayView.layer.cornerRadius = 8
+        
+        premiumImageView.layer.cornerRadius = 10
+        okulImageView.layer.cornerRadius = 10
+        modeImageView.layer.cornerRadius = 10
+        tekImageView.layer.cornerRadius = 10
+        anneImageView.layer.cornerRadius = 10
+        
+        indirimView.layer.cornerRadius = 8
+        fırsatView.layer.cornerRadius = 8
+        indirimView.layer.borderWidth = 1.1
+        fırsatView.layer.borderWidth = 1.1
+        indirimView.layer.borderColor = UIColor(named: "hborange")?.cgColor
+        fırsatView.layer.borderColor = UIColor(named: "hborange")?.cgColor
+        
+        reklamImageView.layer.cornerRadius = 10
+        
+        
         
     }
 
